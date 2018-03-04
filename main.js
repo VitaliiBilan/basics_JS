06ccf7e6886d1b09bd4ad6ffecbd2e08e79ee507
VitaliiBilan <bilan_v@ukr.net>
2018-03-04 16:55:31 +0200

    Homework basics JS

---
+++ main.js
@@ -0,0 +1,30 @@
+alert("Hello user!!!");
+
+
+var firstName = "Alex";
+var lastName = firstName;
+document.write(lastName);
+
+
+var name = 'Vitalii';
+var serName = 'Bilan';
+var age = 31;
+var educationLevel = 'muster degree';
+var hobby = 'coins';
+
+
+//Nan, 1, 4, 4, 2, 3.
+var a = 1;
+var b = 1;
+++c; alert(c);
+var d = b++; alert(d);
+var c = (2+ ++a); alert(c);
+var d = (2+ b++); alert(d);
+alert(a);
+alert(b);
+
+
+var x = +prompt('Number 1', 10);
+var y = +prompt('Number 2', 5);
+res = x + y;
+alert(res);
